7abf740d22a95945a292b67c1701b6f586734644
23Yong <1239824@naver.com>
2023-02-11 23:46:31 +0900

    [init] 프로젝트 최초 커밋

---
+++ src/test/java/com/muclone/musinsaclone/MusinsaCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muclone.musinsaclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
