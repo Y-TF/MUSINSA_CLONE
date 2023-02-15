aecee45814bd8a3cec986bb61e9bda6aeee9fa4f
23Yong <1239824@naver.com>
2023-02-15 22:59:44 +0900

    feat: Brand 엔티티 추가

---
+++ src/main/java/com/muclone/musinsaclone/domain/brand/BrandRepository.java
@@ -0,0 +1,6 @@
+package com.muclone.musinsaclone.domain.brand;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+}
